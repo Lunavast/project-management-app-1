069665e1e31dc4841943c1839b8bc9deed96256f
Rhez <rhezannalbarracin.rc@gmail.com>
2017-01-11 18:10:55 +0800

    Added model and database for Task Done

--- src/home/components/task-item.ts
+++ src/home/components/task-item.ts
@@ -1,5 +1,14 @@
+// 
+// import 'rxjs/add/operator/do';
+// import 'rxjs/add/operator/pluck';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { TaskService } from '../services/task-service';
+// 
+
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { ITask } from '../models/task';
+import { IDoneTask } from '../models/task-done';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -9,7 +18,9 @@ import { ITask } from '../models/task';
     ],
     template:
     `<div class="task-item">
-        <i class="fa fa-check" aria-hidden="true"></i>
+        <i class="fa fa-check" aria-hidden="true"
+           (click)="submitDoneTask()"
+        ></i>
         <i class="fa fa-trash" aria-hidden="true"
             (click)="remove.emit()">
         </i><span>
@@ -19,5 +30,20 @@ import { ITask } from '../models/task';
 })
 export class TaskItemComponent{
     @Input()task: ITask;
+    @Input()taskdone: IDoneTask;
     @Output() remove = new EventEmitter(false);
+    @Output() createDoneTask = new EventEmitter(false);
+
+    filter: Observable<any>;
+    constructor(public route: ActivatedRoute, public taskService: TaskService){
+        this.filter = route.params
+            .pluck('completed')
+            .do((value: string) => taskService.filterDoneTasks(value));
+    }
+
+    submitDoneTask(): void{
+        this.taskService.createDoneTask(this.task.title);
+    }
+
+
 }
--- src/home/services/task-service.ts
+++ src/home/services/task-service.ts
@@ -7,18 +7,25 @@ import { Observable } from 'rxjs/Observable';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { AuthService } from '../../auth';
 import { ITask, Task } from '../models/task';
+import { IDoneTask, DoneTask } from '../models/task-done';
 
 @Injectable()
 export class TaskService{
     visibleTasks$: Observable<ITask[]>;
-    
+
     private filter$: ReplaySubject<any> = new ReplaySubject(1);
     private filteredTasks$: FirebaseListObservable<ITask[]>;
     private tasks$: FirebaseListObservable<ITask[]>; 
 
+    //task-done
+    visibleTasks1$: Observable<IDoneTask[]>; 
+
+    private filter1$: ReplaySubject<any> = new ReplaySubject(2);
+    private filteredTasks1$: FirebaseListObservable<IDoneTask[]>;
+    private tasks1$: FirebaseListObservable<IDoneTask[]>;
+
     constructor(af: AngularFire, auth: AuthService){
         const path = `/tasks/${auth.id}`;
-        
         this.tasks$ = af.database.list(path);
 
         this.filteredTasks$ = af.database.list(path, {query: {
@@ -28,7 +35,18 @@ export class TaskService{
 
         this.visibleTasks$ = this.filter$
             .switchMap(filter => filter === null ? this.tasks$ : this.filteredTasks$);
-    }
+        
+        //task-done 
+        const path1 = `/tasksdone/${auth.id}`;
+        this.tasks1$ = af.database.list(path1);
+        this.filteredTasks1$ = af.database.list(path1, {query: {
+                orderByChild: 'completed',
+                equalTo: this.filter1$
+        }});
+        this.visibleTasks1$ = this.filter1$
+            .switchMap(filter_d => filter_d === null ? this.tasks1$ : this.filteredTasks1$);
+
+}
     filterTasks(filter: string): void{
         switch(filter){
             case 'false':
@@ -42,13 +60,35 @@ export class TaskService{
                 break;
         }
     }
+    
+     //task-done 
+    filterDoneTasks(filter: string): void{
+            switch (filter) {
+                case 'false':
+                    this.filter1$.next(false);
+                    break;
+                case 'true':
+                    this.filter1$.next(true);
+                default:
+                    this.filter1$.next(null);
+                    break;
+            }
+    }
+
     createTask(title: string): firebase.Promise<any>{
         return this.tasks$.push(new Task(title));
     }
     removeTask(task: ITask): firebase.Promise<any>{
         return this.tasks$.remove(task.$key);
-   }
-   updateTask(task: ITask, changes: any): firebase.Promise<any>{
+    }
+    updateTask(task: ITask, changes: any): firebase.Promise<any>{
        return this.tasks$.update(task.$key, changes);
-   }
+    }
+
+    //task-done 
+    createDoneTask(title: string): firebase.Promise<any>{
+        return  this.tasks1$.push(new DoneTask(title));
+    }
+
+
 }
